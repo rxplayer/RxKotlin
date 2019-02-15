feb856e6bec4e8acba5fe2383553dc1676940b62
YuJaeMan <yooman22@naver.com>
2019-02-15 11:34:24 +0900

    예제 추가

--- app/src/main/java/com/example/mutecsoft/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/mutecsoft/myapplication/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 버튼 클릭 이벤트 예제////////////////////////////////////////////
         val inc = button1.clicks()
             .map { +1 }
 
@@ -29,22 +30,80 @@ class MainActivity : AppCompatActivity() {
             .scan(0) { acc: Int, value: Int -> acc + value }
             .subscribe { text.text = it.toString() }
         
-
+        // map 예제//////////////////////////////////////////////////////////
         val list = listOf("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
 
         list.toObservable() // extension function for Iterables
             .subscribeOn(Schedulers.io())
-            .filter { it.length >= 5 }
-            .map { it -> "Delta" }
+            .filter { it.length >= 5 } // 시간순으로 들어온 데이터를 걸러주는 거
+            .map { it -> "Delta" } // 원하는 값으로 변환하는 함수
             .observeOn(Schedulers.newThread())
             .subscribe {
                 edit.setText(it)
             }
 
+        edit.textChanges()
+            .map { it -> it}
+            .subscribe { text.setText(it) }
+
+        val array : Array <String> = arrayOf ( "allow" , "bow" , "" , "dead" )
+        Observable.just(array)
+            .map { it-> it.size }
+            . subscribe ( {
+                println (it)
+            }, {} )
+
+        //flatMap//////////////////////////////////////////////////////////////
+        val array1 : Array <String> = arrayOf ( "allow" , "bow" , "" , "dead" )
+        array1.toObservable()
+            .filter { it.length > 4 }
+            .flatMap { i -> if ( i == "bow" ) Observable.just ( "fuck" ) else Observable.just (i.length) }
+            .subscribe ( {
+                println (it)
+            } , {} )
+
+        //filter //////////////////////////////////////////////////////////////
+
+        //<비슷한 기능을 하는 함수> ///////////////////////////////////////////
+        //first(default) : Objservable의 첫 번째 값을 필터함, 만약 값이 없으면 기본값을 리턴
+//        last(default) : Objservable의 마지막 값을 필터함, 만약 값이 없으면 기본값을 리턴
+//        take(N) : 최초 N개 값을 가져옴
+//        takeLast(N) : 마지막 N개 값을 가져옴
+//        skip(N) : 최초 N개 값을 건너뜀
+//        skipLast(N) : 마지막 N개 값을 건너
+
+
+        val array2 : Array <String> = arrayOf ( "allow" , "bow" , "" , "dead" )
+        array2.toObservable()
+            .filter { ! it.isEmpty ( ) }
+            .subscribe ( {
+                println (it)
+            } , {} )
+
 
         Observable.just(2, 30, 22, 5, 60, 1)
             .filter { x -> x > 10 }
             .subscribe { x-> Log.d("안녕","item :" + x) }
+
+        // skip //////////////////////////////////////////////////////////////
+        val array3 : Array <String> = arrayOf ( "allow" , "bow" , "community" , "dead" )
+        array3.toObservable()
+            .skip ( 2 )
+            .subscribe ( {
+                println (it)
+            }, {} )
+
+        val array4 : Array <String> = arrayOf ( "allow" , "bow" , "" , "dead" )
+        array4.toObservable()
+            .take ( 2 )
+            .subscribe ( {
+                println (it)
+            }, {} )
+
+
+
+
+
 
 
         val c = Calendar.getInstance()
